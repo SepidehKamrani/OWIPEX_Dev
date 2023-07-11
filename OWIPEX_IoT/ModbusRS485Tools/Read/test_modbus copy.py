d9777fa7008c148fcbf8612e94ac6998b63e246d
KarimSayedAmir <s.karim@wer.ag>
2023-07-12 01:23:11 +0200

    Working Main

--- OWIPEX_IoT/ModbusRS485Tools/Read/test_modbus copy.py
+++ OWIPEX_IoT/ModbusRS485Tools/Read/test_modbus copy.py
@@ -28,12 +28,12 @@ Trub_Sensor = dev_manager.get_device(device_id=0x02)
 # Read temperatures
 tempPHSens = PH_Sensor.read_register(start_address=0x0003, register_count=2)
 tempTruebSens = Trub_Sensor.read_register(start_address=0x0003, register_count=2)
-#print(f'Temperature 1: {temp1}, Temperature 2: {temp2}')
+print(f'tempPHSens: {tempPHSens}, tempTruebSens: {tempTruebSens}')
 
 # Read other values
-PH = PH_Sensor.read_register(start_address=0x0001, register_count=2)
-Treub = Trub_Sensor.read_register(start_address=0x0001, register_count=2)
-#print(f'Other Value 1: {other_value1}, Other Value 2: {other_value2}')
+PHValue = PH_Sensor.read_register(start_address=0x0001, register_count=2)
+TruebValue = Trub_Sensor.read_register(start_address=0x0001, register_count=2)
+print(f'PH Wert: {PHValue}, Truebung: {TruebValue}')
 
 # To stop auto read
 # dev_manager.auto_read = False
