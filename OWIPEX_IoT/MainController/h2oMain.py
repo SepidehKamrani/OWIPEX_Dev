2be7998bbcb57515c291eb33830b234e1735ebaa
KarimSayedAmir <s.karim@wer.ag>
2023-07-12 01:22:12 +0200

    Working Sensor Read from h2oMain

--- OWIPEX_IoT/MainController/h2oMain.py
+++ OWIPEX_IoT/MainController/h2oMain.py
@@ -4,12 +4,25 @@ import os
 import random
 
 from tb_gateway_mqtt import TBDeviceMqttClient
+from modbus_lib import DeviceManager
+from time import sleep
 
-ACCESS_TOKEN = "QgcgigdZyC3Wladd40Be"  # Replace this with your actual access token
+ACCESS_TOKEN = "I9Vbnng0MBpxY5UDX67l"  # Replace this with your actual access token
 THINGSBOARD_SERVER = 'localhost'  # Replace with your Thingsboard server address
 THINGSBOARD_PORT = 1883
 
-logging.basicConfig(level=logging.DEBUG)
+
+#RS485 Comunication and Devices
+# Create DeviceManager
+dev_manager = DeviceManager(port='/dev/ttymxc3', baudrate=9600, parity='N', stopbits=1, bytesize=8, timeout=1)
+
+# Add devices
+dev_manager.add_device(device_id=0x01)
+dev_manager.add_device(device_id=0x02)
+
+
+
+#logging.basicConfig(level=logging.DEBUG)
 
 client = None
 
@@ -38,6 +51,9 @@ tankVolumeToMaxAllowedFill = ""
 calculatedFlowRate = ""
 powerSwitch = ""
 automaticSwitch = ""
+temperaturPHSens_telem = 0
+measuredPHValue_telem = 0
+measuredTurbidity_telem = 0
 
 # Callback function that will be called when the value of our Shared Attribute changes
 def attribute_callback(result, _):
@@ -80,6 +96,7 @@ def rpc_callback(id, request_body):
         print('Unknown method: ' + method)
 
 def get_data():
+    global temperaturPHSens_telem, measuredPHValue_telem, measuredTurbidity_telem
     cpu_usage = round(float(os.popen('''grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage }' ''').readline().replace('\n', '').replace(',', '.')), 2)
     ip_address = os.popen('''hostname -I''').readline().replace('\n', '').replace(',', '.')[:-1]
     mac_address = os.popen('''cat /sys/class/net/*/address''').readline().replace('\n', '').replace(',', '.')
@@ -95,15 +112,11 @@ def get_data():
     calculatedFlowRate_telem = random.uniform(15, 25)
     alarmActive_telem = False
     waterLevelHeight_telem = random.uniform(180, 200)
-    turbiditySensorActive_telem = True
+    #turbiditySensorActive_telem = True
     measuredTurbidity_telem = random.uniform(100, 200)
-    measuredPHValue_telem = random.uniform(5, 7)
+    #measuredPHValue_telem = random.uniform(5, 7)
     alarmOverfill_telem = False
-    temperaturPHSens_telem = random.uniform(-5, 20)
-
-
-
-    
+    #temperaturPHSens_telem = random.uniform(-5, 20)
 
     attributes = {
         'ip_address': ip_address,
@@ -134,7 +147,7 @@ def get_data():
     return attributes, telemetry
 
 def main():
-    global client
+    global client, temperaturPHSens_telem, measuredPHValue_telem, measuredTurbidity_telem
     client = TBDeviceMqttClient(THINGSBOARD_SERVER, THINGSBOARD_PORT, ACCESS_TOKEN)
     client.connect()
     
@@ -176,7 +189,31 @@ def main():
         attributes, telemetry = get_data()
         client.send_attributes(attributes)
         client.send_telemetry(telemetry)
-        time.sleep(60)
+
+        # Get devices and read their registers
+        PH_Sensor = dev_manager.get_device(device_id=0x01)
+        Trub_Sensor = dev_manager.get_device(device_id=0x02)
+
+        try:
+            # Start auto read
+            # Read temperatures
+            temperaturPHSens_telem = PH_Sensor.read_register(start_address=0x0003, register_count=2)
+            tempTruebSens = Trub_Sensor.read_register(start_address=0x0003, register_count=2)
+            print(f'Temperature 1: {temperaturPHSens_telem}, Temperature 2: {tempTruebSens}')
+
+            # Read other values
+            measuredPHValue_telem = PH_Sensor.read_register(start_address=0x0001, register_count=2)
+            measuredTurbidity_telem = Trub_Sensor.read_register(start_address=0x0001, register_count=2)
+            print(f'Other Value 1: {measuredPHValue_telem}, Other Value 2: {measuredTurbidity_telem}')
+            # Stop auto read
+            #PH_Sensor.stop_auto_read()
+            #Trub_Sensor.stop_auto_read()
+            
+        except Exception as e:
+            print(f"An error occurred: {e}")
+        #END RS485
+
+        time.sleep(5)
 
 if __name__ == '__main__':
     if ACCESS_TOKEN != "TEST_TOKEN":
