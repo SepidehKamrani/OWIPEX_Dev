09606ce31e7528a29e0269115cfa0d3eb1fd052b
KarimSayedAmir <s.karim@wer.ag>
2023-07-18 13:20:27 +0200

    phControl implementation

---
+++ OWIPEX_IoT/PHControl/ph_control.py
@@ -0,0 +1,105 @@
+"""
+# -----------------------------------------------------------------------------
+# Company: KARIM Technologies
+# Author: Sayed Amir Karim
+# Copyright: 2023 KARIM Technologies
+#
+# License: All Rights Reserved
+#
+# Module: PHControl
+# Version: 1.0
+# Description: Dieses Modul stellt die PHControl-Klasse bereit, die eine
+# automatisierte Kontrolle des pH-Wertes in einem System ermöglicht. Sie
+# ermöglicht die automatische Regelung eines CO2-Ventils und einer Pumpe
+# basierend auf dem gemessenen pH-Wert und Benutzereingaben. Die Klasse nutzt
+# dabei die GPIOControl-Klasse, um die GPIOs zu steuern.
+#
+# Die Klasse verfügt über folgende Methoden zur Interaktion:
+#
+# - `set_measured_ph(ph)`: Setzt den aktuell gemessenen pH-Wert.
+# - `set_pump_switch(switch)`: Steuert den Schalter für die Pumpe. Wenn `switch`
+# auf `True` gesetzt ist, wird die Pumpe eingeschaltet, und wenn `switch` auf
+# `False` gesetzt ist, wird die Pumpe ausgeschaltet.
+# - `set_co2_valve_switch(switch)`: Erlaubt die manuelle Steuerung des CO2-Ventils.
+# Wenn `switch` auf `True` gesetzt ist, wird das CO2-Ventil eingeschaltet, und wenn
+# `switch` auf `False` gesetzt ist, wird das CO2-Ventil ausgeschaltet.
+#
+# Das Modul startet einen Hintergrundthread, der kontinuierlich die Systemparameter
+# überprüft und entsprechend die Geräte steuert. Die manuelle Steuerung der Geräte
+# hat dabei Vorrang vor der automatischen Steuerung basierend auf dem gemessenen pH-Wert.
+# -----------------------------------------------------------------------------
+"""
+# ph_control.py
+import time
+import threading
+from gpiocontrol import GPIOControl
+
+class PHControl:
+    def __init__(self, min_ph, max_ph, check_timer, on_delay_timer):
+        self.min_ph = min_ph
+        self.max_ph = max_ph
+        self.check_timer = check_timer
+        self.on_delay_timer = on_delay_timer
+        self.measured_ph = None
+        self.pump_switch = False
+        self.co2_valve_switch = None  # Neue Variable für die manuelle CO2-Ventilsteuerung
+        self.co2_valve = GPIOControl(6)  # CO2-Ventil ist auf GPIO 6
+        self.pump = GPIOControl(7)  # Pumpe ist auf GPIO 7
+        self.co2_valve.set_direction('out')
+        self.pump.set_direction('out')
+
+        # Alle GPIOs zur Sicherheit auf "Aus" setzen
+        self.co2_valve.set_value('1')
+        self.pump.set_value('1')
+
+        # Starte den Hintergrundthread
+        threading.Thread(target=self.control_loop, daemon=True).start()
+
+    def set_measured_ph(self, ph):
+        self.measured_ph = ph
+
+    def set_pump_switch(self, switch):
+        self.pump_switch = switch
+
+    def set_co2_valve_switch(self, switch):
+        self.co2_valve_switch = switch
+
+    def control_loop(self):
+        while True:
+            # Steuerung der Pumpe
+            if self.pump_switch:
+                self.pump.set_value('0')  # Pumpe ein
+            else:
+                self.pump.set_value('1')  # Pumpe aus
+
+            # Manuelle Steuerung des CO2-Ventils
+            if self.co2_valve_switch is not None:
+                if self.co2_valve_switch:
+                    self.co2_valve.set_value('0')  # CO2-Ventil ein
+                else:
+                    self.co2_valve.set_value('1')  # CO2-Ventil aus
+            # Automatische Steuerung des CO2-Ventils
+            elif self.measured_ph is not None and self.measured_ph > self.max_ph:
+                time.sleep(self.on_delay_timer)
+
+                # Nach der Wartezeit, überprüfen wir erneut den PH-Wert
+                if self.measured_ph > self.max_ph:
+                    # CO2 Ventil einschalten (0 = Ein)
+                    self.co2_valve.set_value('0')
+
+                    # Kontinuierliche Überprüfung, ob der PH-Wert sinkt
+                    while self.measured_ph > self.max_ph:
+                        time.sleep(1)
+
+                    # Sobald der PH-Wert unter dem max_ph liegt, starten wir den check_timer
+                    start_time = time.time()
+                    while time.time() - start_time < self.check_timer:
+                        # Wenn der PH-Wert während der check_timer Zeit wieder steigt, brechen wir ab und starten von vorne
+                        if self.measured_ph > self.max_ph:
+                            break
+                        time.sleep(1)
+                    else:
+                        # CO2 Ventil ausschalten (1 = Aus)
+                        self.co2_valve.set_value('1')
+
+            time.sleep(1)
---
+++ OWIPEX_IoT/PHControl/testPHControl.py
@@ -0,0 +1,43 @@
+"""
+# -----------------------------------------------------------------------------
+# Company: KARIM Technologies
+# Author: Sayed Amir Karim
+# Copyright: 2023 KARIM Technologies
+#
+# License: All Rights Reserved
+#
+# Module: MainProgram
+# Version: 1.0
+# Description: Dies ist das Hauptprogramm, das die PHControl und GPIOControl Module verwendet, 
+# um den pH-Wert in einem System automatisch zu regulieren und eine Pumpe zu steuern. Es bietet 
+# auch eine Schnittstelle für die manuelle Steuerung des Systems.
+#
+# Im Hauptprogramm werden drei Werte gehandhabt:
+# - `minimumPHValue`: Der minimale zulässige pH-Wert.
+# - `maximumPHValue`: Der maximale zulässige pH-Wert.
+# - `measuredPHValue_telem`: Der gemessene pH-Wert, der vom Telemetriesystem geliefert wird.
+#
+# Das Hauptprogramm stellt Schnittstellen zur Verfügung, um diese Werte zu setzen und zu erhalten, 
+# und es verwendet die PHControl Klasse, um die automatische Kontrolle des Systems basierend auf 
+# diesen Werten zu handhaben. Es bietet auch Schnittstellen für die manuelle Steuerung des CO2-Ventils 
+# und der Pumpe.
+# -----------------------------------------------------------------------------
+"""
+
+# main.py
+from ph_control import PHControl
+
+# Erstellt ein PHControl-Objekt
+ph_control = PHControl(min_ph=5.0, max_ph=7.0, check_timer=60, on_delay_timer=60)
+
+# Aktualisieren Sie den gemessenen pH-Wert in Ihrem Hauptprogramm
+# Dies sollte in Ihrem tatsächlichen Code durch eine tatsächliche pH-Messung ersetzt werden
+ph_control.set_measured_ph(8.0)
+
+# Setzen Sie den Pumpenschalter in Ihrem Hauptprogramm
+# Dies sollte in Ihrem tatsächlichen Code durch eine tatsächliche Benutzereingabe oder andere Logik ersetzt werden
+ph_control.set_pump_switch(True)
+
+# Setzen Sie den CO2-Ventilschalter in Ihrem Hauptprogramm
+# Dies sollte in Ihrem tatsächlichen Code durch eine tatsächliche Benutzereingabe oder andere Logik ersetzt werden
+ph_control.set_co2_valve_switch(True)
