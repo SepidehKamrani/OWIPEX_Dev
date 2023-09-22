2c2a19aefe4c0b1db43c3fc0cc13fdf5facddeaf
KarimSayedAmir <s.karim@wer.ag>
2023-09-22 22:24:40 +0200

    adopted , 'minimumPHValue', 'maximumPHValue' to shared

--- OWIPEX_IoT/V1.0/h2o copy.py
+++ OWIPEX_IoT/V1.0/h2o copy.py
@@ -361,6 +361,9 @@ def main():
                     co2HeatingRelaySw = False
             else:
                 print("automode OFF", autoSwitch)
+                pumpRelaySw = False
+                co2RelaisSw = False
+                co2HeatingRelaySw = False
                 ph_low_delay_start_time = None
                 ph_high_delay_start_time = None
                 countdownPHHigh = ph_high_delay_duration
