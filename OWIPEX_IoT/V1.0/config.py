54cd4a3bf5a287205b9ee4f2185c17778261804c
KarimSayedAmir <s.karim@wer.ag>
2023-09-22 19:14:31 +0200

    chages for easy

--- OWIPEX_IoT/V1.0/config.py
+++ OWIPEX_IoT/V1.0/config.py
@@ -64,11 +64,10 @@ gpsHeight = 1.0
 
 # Telemetry and Attribute Variables
 telemetry_keys = ['powerButton', 'autoSwitch', 'co2RelaisSw', 'co2HeatingRelaySw', 'pumpRelaySw', 'calculatedFlowRate', 'waterLevelHeight_telem', 'measuredTurbidity_telem', 'co2RelaisSwSig', 
-                  'co2HeatingRelaySwSig', 'pumpRelaySwSig', 'measuredPHValue_telem', 'temperaturPHSens_telem', 
-                  'gpsTimestamp', 'messuredRadar_Air_telem', 
-                  'countdownPHHigh', 'countdownPHLow', 'flow_rate_l_min', 'flow_rate_l_h', 'flow_rate_m3_min']
+                  'co2HeatingRelaySwSig', 'pumpRelaySwSig', 'measuredPHValue_telem', 'temperaturPHSens_telem', 'gpsTimestamp', 'messuredRadar_Air_telem', 
+                  'countdownPHHigh', 'countdownPHLow', 'flow_rate_l_min', 'flow_rate_l_h', 'flow_rate_m3_min', 'gpsLatitude', 'gpsLongitude', 'gpsHeight']
 
-attributes_keys = ['ip_address', 'macaddress', 'gpsLatitude', 'gpsLongitude', 'gpsHeight']
+attributes_keys = ['ip_address', 'macaddress']
 
 
 
@@ -78,8 +77,7 @@ shared_attributes_keys = ['maximumTurbidity', 'turbiditySensorActive', 'turbidit
 
 machine_attributes_keys = ['autoSwitch', 'powerButton', 'co2RelaisSw', 'pumpRelaySw', 'co2HeatingRelaySw']
 
-ph_attributes_keys = ['ph_low_delay_start_time', 'ph_high_delay_duration', 
-                      'PHValueOffset', 'gemessener_high_wert', 'gemessener_low_wert', 'calibratePH', 'targetPHValue', 'targetPHtolerrance', 'minimumPHValue', 'maximumPHValue']
+ph_attributes_keys = ['ph_low_delay_start_time', 'ph_high_delay_duration', 'PHValueOffset', 'gemessener_high_wert', 'gemessener_low_wert', 'calibratePH', 'targetPHValue', 'targetPHtolerrance', 'minimumPHValue', 'maximumPHValue']
 
 turbidity_attributes_keys = ['maximumTurbidity', 'turbiditySensorActive', 'turbidityOffset', 'tempTruebSens']
 
