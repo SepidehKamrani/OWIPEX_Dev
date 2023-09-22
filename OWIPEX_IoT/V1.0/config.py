2cb502af25f611dc4af29afb06da98ff65ae9c0d
KarimSayedAmir <s.karim@wer.ag>
2023-09-22 22:51:07 +0200

    deleted targetPHValue und targetPHtolerrance from telemetry

--- OWIPEX_IoT/V1.0/config.py
+++ OWIPEX_IoT/V1.0/config.py
@@ -65,7 +65,7 @@ gpsHeight = 1.0
 # Telemetry and Attribute Variables
 telemetry_keys = ['powerButton', 'autoSwitch', 'co2RelaisSw', 'co2HeatingRelaySw', 'pumpRelaySw', 'calculatedFlowRate', 'waterLevelHeight_telem', 'measuredTurbidity_telem', 'co2RelaisSwSig', 
                   'co2HeatingRelaySwSig', 'pumpRelaySwSig', 'measuredPHValue_telem', 'temperaturPHSens_telem', 'gpsTimestamp', 'messuredRadar_Air_telem', 
-                  'countdownPHHigh', 'countdownPHLow', 'flow_rate_l_min', 'flow_rate_l_h', 'flow_rate_m3_min', 'gpsLatitude', 'gpsLongitude', 'gpsHeight', 'minimumPHValue', 'maximumPHValue', 'ph_low_delay_start_time', 'ph_high_delay_duration']
+                  'countdownPHHigh', 'countdownPHLow', 'flow_rate_l_min', 'flow_rate_l_h', 'flow_rate_m3_min', 'gpsLatitude', 'gpsLongitude', 'gpsHeight', 'ph_low_delay_start_time', 'ph_high_delay_duration']
 
 attributes_keys = ['ip_address', 'macaddress']
 
