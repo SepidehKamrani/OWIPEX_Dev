3b8b611b0413ffaffe78340085205c05dcc47171
KarimSayedAmir <s.karim@wer.ag>
2023-11-01 20:09:39 +0100

    added debuggib phcalib

--- OWIPEX_IoT/V1.0/h2o_persistant_test.py
+++ OWIPEX_IoT/V1.0/h2o_persistant_test.py
@@ -304,9 +304,16 @@ def main():
                 print(f"Flow Rate (Liters per Hour): {flow_data['flow_rate_l_h']} L/h")
                 print(f"Flow Rate (Cubic Meters per Minute): {flow_data['flow_rate_m3_min']} m3/min")
 
+        print("Vor der Kalibrierung:")
+        print("Steigung (slope):", ph_handler.slope)
+        print("y-Achsenabschnitt (intercept):", ph_handler.intercept)
+
         if calibratePH:
             ph_handler.calibrate(high_ph_value=10, low_ph_value=7, measured_high=gemessener_high_wert, measured_low=gemessener_low_wert)
             calibratePH = False
+            print("Nach der Kalibrierung:")
+            print("Steigung (slope):", ph_handler.slope)
+            print("y-Achsenabschnitt (intercept):", ph_handler.intercept)
         else:
             measuredPHValue_telem, temperaturPHSens_telem = ph_handler.fetch_and_display_data()  
             measuredTurbidity_telem, tempTruebSens = turbidity_handler.fetch_and_display_data(turbiditySensorActive)
